d3fcf8f9ad7ae3111d2888c53db8b1b7c6211bcb
begidazu <benat.egidazu@bc3research.org>
2025-07-09 00:49:43 +0200

    App requirements simplification. No Docker

---
+++ app/models/model_b.py
@@ -0,0 +1,5 @@
+# app/models/model_b.py
+
+def run():
+    # Lógica de Model B
+    return {"message": "Resultado de Model B"}
--- dash_app/app/models/model_b.py
+++
@@ -1,3 +0,0 @@
-def run():
-    # Lógica de Model A (cargar datos, entrenar, predecir, etc.)
-    return "Resultado de Model A"
