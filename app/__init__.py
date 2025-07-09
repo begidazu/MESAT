8b893521603148406e7274dc04259afccc6e4004
begidazu <benat.egidazu@bc3research.org>
2025-07-09 11:46:55 +0200

    Hide Error log

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,10 @@ from .callbacks.models_callbacks import register_model_callbacks
 from .callbacks.draw_callbacks import register_draw_callbacks
 
 def create_app():
-    app = Dash(__name__)
+    app = Dash(
+        __name__, 
+        title = "PhD Web Application"
+        )
     app.layout = create_layout()
     register_model_callbacks(app)
     register_draw_callbacks(app)
--- run.py
+++ run.py
@@ -3,4 +3,9 @@ from app import create_app
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=8050)
+    app.run(
+        debug=True,
+        host="0.0.0.0",
+        port=8050,
+        dev_tools_ui=False,            # quita el botón azul de errores
+        dev_tools_props_check=False)
