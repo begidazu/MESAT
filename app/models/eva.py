ed0fd74a28316ba4049b1b9209e9ac9d68f4f332
begidazu <benat.egidazu@bc3research.org>
2025-09-05 10:37:55 +0200

    Overscale EVA AQ1 improved

--- app/models/eva.py
+++ app/models/eva.py
@@ -13,6 +13,7 @@ from shapely.geometry import box, Point, Polygon, MultiPolygon, GeometryCollecti
 from shapely import wkb, wkt
 import matplotlib.pyplot as plt
 from pyobis.occurrences import occurrences
+from pyobis import taxa
 try:
     from shapely.validation import make_valid   # Shapely ≥2.0
     _has_make_valid = True
@@ -38,30 +39,6 @@ def best_utm_crs(file:str) -> CRS:
     epsg = 32600 + zone if lat >= 0 else 32700 + zone
     return CRS.from_epsg(epsg)
 
-# Function to create a regular grid:
-def create_grid(gdf = None, bounds=None, grid_size = 100):
-    """Create square grid that covers a geodataframe area
-    or a fixed boundary with x-y coords
-    returns: a GeoDataFrame of grid polygons
-    """
-    # bounds:
-    xmin, ymin, xmax, ymax= bounds
-    # cell size
-    cell_size = grid_size
-    # Geodataframe CRS:
-    crs = gdf.crs
-    # create the cells in a loop
-    grid_cells = []
-    for x0 in np.arange(xmin, xmax+cell_size, cell_size ):
-        for y0 in np.arange(ymin, ymax+cell_size, cell_size):
-            x1 = x0-cell_size
-            y1 = y0+cell_size
-            poly = shapely.geometry.box(x0, y0, x1, y1)
-            #print (gdf.overlay(poly, how='intersection'))
-            grid_cells.append( poly )
-    cells = gpd.GeoDataFrame(grid_cells, columns=['geometry'], crs=crs)
-    return cells
-
 # Funcion para quedarnos con los poligonos de un Geodataframe que intersecan con otro Geodataframe
 def keep_intersecting(gdf1: gpd.GeoDataFrame, gdf2: gpd.GeoDataFrame) -> gpd.GeoDataFrame:
     # 1) Sanea geometrías (evita TopologyException)
@@ -74,9 +51,58 @@ def keep_intersecting(gdf1: gpd.GeoDataFrame, gdf2: gpd.GeoDataFrame) -> gpd.Geo
     hits = gpd.sjoin(g1, g2[["geometry"]], how="inner", predicate="intersects")
     return g1.loc[hits.index.unique()].copy() 
 
+# Function to create a regular grid:
+def create_grid(aoi = None, grid_size = 1000):
+    """Create square grid that covers the area of interest
+    returns: a GeoDataFrame of grid polygons
+    """
+
+    # We add a conditional to check file format
+    if not (aoi.endswith(".json") or aoi.endswith(".parquet") or aoi.endswith(".geojson")):
+        raise ValueError("The selected file is not a .json or .parquet file!")
+    
+    # Read the file:
+    if aoi.endswith(".parquet"):
+        aoi_gdf= gpd.read_parquet(aoi)
+    elif aoi.endswith(".json"):
+        aoi_gdf = gpd.read_file(aoi)
+    elif aoi.endswith(".geojson"):
+        aoi_gdf = gpd.read_file(aoi)
+
+
+    # Ensure a projected CRS (meters)
+    if aoi_gdf.crs.is_projected:
+        gdf_m = aoi_gdf
+        metric_crs = aoi_gdf.crs
+    else:
+        metric_crs = best_utm_crs(aoi)
+        gdf_m = aoi_gdf.to_crs(metric_crs)
+
+    # bounds:
+    min_x, min_y, max_x, max_y = gdf_m.total_bounds
+    bounds = (min_x, min_y, max_x, max_y)
+
+    # cell size
+    cell_size = grid_size
+    # Geodataframe CRS:
+    crs = aoi_gdf.crs
+    # create the cells in a loop
+    grid_cells = []
+    for x0 in np.arange(min_x, max_x+cell_size, cell_size ):
+        for y0 in np.arange(min_y, max_y+cell_size, cell_size):
+            x1 = x0-cell_size
+            y1 = y0+cell_size
+            poly = shapely.geometry.box(x0, y0, x1, y1)
+            #print (gdf.overlay(poly, how='intersection'))
+            grid_cells.append( poly )
+    cells = gpd.GeoDataFrame(grid_cells, columns=['geometry'], crs=metric_crs)
+
+    filtered_cells = keep_intersecting(cells, gdf_m)
+    return filtered_cells
+
 def to_multipolygon(obj: Union[gpd.GeoDataFrame, gpd.GeoSeries, Polygon, MultiPolygon, GeometryCollection, Iterable]) -> MultiPolygon:
-    """Devuelve siempre un MultiPolygon a partir de Polygon/MultiPolygon/Geo(Series|DataFrame)/lista."""
-    # 1) recopila geometrías
+    """Returns always a MultiPolygon from Polygon/MultiPolygon/Geo(Series/Dataframe)/list."""
+    # 1) Geometries
     if isinstance(obj, gpd.GeoDataFrame):
         geoms = obj.geometry
     elif isinstance(obj, gpd.GeoSeries):
@@ -88,7 +114,7 @@ def to_multipolygon(obj: Union[gpd.GeoDataFrame, gpd.GeoSeries, Polygon, MultiPo
     else:
         raise TypeError("Objeto no soportado para to_multipolygon().")
 
-    # 2) sanea y conserva solo polígonos
+    # 2) Sanitize and keep just the polygons
     polys = []
     for g in geoms:
         if g is None:
@@ -113,7 +139,7 @@ def to_multipolygon(obj: Union[gpd.GeoDataFrame, gpd.GeoSeries, Polygon, MultiPo
     if not polys:
         return MultiPolygon([])
 
-    # 3) disolver y asegurar MultiPolygon (compatible con versiones)
+    # 3) Dissolve and ensure Multipolygon
     try:
         from shapely import union_all as shp_union_all  # Shapely 2.x rápido
         merged = shp_union_all(polys)
@@ -136,15 +162,35 @@ def to_multipolygon(obj: Union[gpd.GeoDataFrame, gpd.GeoSeries, Polygon, MultiPo
     return MultiPolygon(parts) if parts else MultiPolygon([])
 
 
+
 # --------------------- EVA Assessment Questions ----------------------------------------------------
+
+# Assessment Question 1: presence of Locally Rare Features
 def aq1(
      aoi: str,                       # ruta al area de interes (json o parquet)
      species: List[str],             # list of species names
-     grid_size: int,                  # size of the rectangular grid in meters  
+    #  grid_size: int,                  # size of the rectangular grid in meters
+     grid: gpd.GeoDataFrame,
      min_grid_per: int,             # minimum percercentage of grids that need to have data to do the assessment (computed with each species).
      cut_lrf: int,                   # threshold percentage used to define a species as Locally Rare Species. Less or equal to this threshold will be defined as Locally Rare.
      span_years: int                  # Time span of the occurrence data for the assesment in years
     ) -> gpd.GeoDataFrame:
+
+    """
+    This function computes the EVA Assessment Question 1 from OBIS ocurrence data based on the list of species specified by the user in the area of interest and adds the indicator score to the grid passes by the user.
+
+        Params:
+            aoi: area of interest. It has to be in json, geojson or parquet format.
+            species: a list of species with the species/genus/family names to take into acount in the assessment. Example: ["Spartina", "Halimione portulacoides", "Anas"].
+            grid: grid where the assessment will be conducted and the indicator added.
+            min_grid_per: minimum percercentage of grids that need to have data to do the assessment (computed with each species).
+            cut_lrf: threshold percentage used to define a species as Locally Rare Species. Less or equal to this threshold will be defined as Locally Rare.
+            span_years: Time span of the occurrence data for the assesment in years.
+
+        Returns:
+        A gpd.GeoDataFrame with the grid passed by the user and a columns 'aq1' with the Locally Rare Feature condition indicator.
+
+    """
 
     # We add a conditional to check file format
     if not (aoi.endswith(".json") or aoi.endswith(".parquet") or aoi.endswith(".geojson")):
@@ -160,23 +206,11 @@ def aq1(
 
     # Ensure a projected CRS (meters)
     if aoi_gdf.crs.is_projected:
-        gdf_m = aoi_gdf
+        #gdf_m = aoi_gdf
         metric_crs = aoi_gdf.crs
     else:
         metric_crs = best_utm_crs(aoi)
-        gdf_m = aoi_gdf.to_crs(metric_crs)
-
-    # Get bounding box of the file
-    min_x, min_y, max_x, max_y = gdf_m.total_bounds
-    bounds = (min_x, min_y, max_x, max_y)
-
-    # Create the grid for the assessment:
-    grid = create_grid(gdf = gdf_m, bounds=bounds, grid_size= grid_size)
-
-    # Clean the grid saving those grids that intersect with the aoi:
-    filtered_grid = keep_intersecting(grid, gdf_m)
-
-    filtered_grid.to_file(r"C:\Users\beñat.egidazu\Desktop\Tests\EVA\filtered_grid.shp")
+        #gdf_m = aoi_gdf.to_crs(metric_crs)
 
     # Set up the occurrence data start and end date:
     end_date = datetime.now()
@@ -193,7 +227,7 @@ def aq1(
     lrs_array = []
 
     # Create a column where we will aggregate the EV values of Locally Rare Species:
-    filtered_grid["aggregation"] = 0
+    grid["aggregation"] = 0
 
     # Bucle para iterar sobre la lista de especies y definir si son LRF o no:
     for specie in species:
@@ -220,28 +254,28 @@ def aq1(
             occ_gdf_proj = occ_gdf.to_crs(metric_crs)
 
             # Grid intersecting with occurrence:
-            intersect = gpd.sjoin(filtered_grid, occ_gdf_proj[["geometry"]], how="inner", predicate="intersects")
-            occ_grid = filtered_grid.loc[intersect.index.unique()].copy()
+            intersect = gpd.sjoin(grid, occ_gdf_proj[["geometry"]], how="inner", predicate="intersects")
+            occ_grid = grid.loc[intersect.index.unique()].copy()
 
             # If the percentage is less than the threshold to do the assessment pass:
-            print(f"Numero de grids {specie}:{len(filtered_grid)}; Numero de grids con occurrence data {specie}: {len(occ_grid)}")
-            if ((len(occ_grid)/len(filtered_grid))*100) < min_grid_per:
+            print(f"Numero de grids {specie}:{len(grid)}; Numero de grids con occurrence data {specie}: {len(occ_grid)}")
+            if ((len(occ_grid)/len(grid))*100) < min_grid_per:
                 print(f"El porcentaje de celdas con datos es demasiado pequeno para hacer el assessment con {specie}. Continuamos con la siguinte especie!")
                 pass
-            elif ((len(occ_grid)/len(filtered_grid))*100) >= min_grid_per:
+            elif ((len(occ_grid)/len(grid))*100) >= min_grid_per:
                 print(f"El porcentaje de celdas con datos es adecuado, la especie {specie} se incluye en el assessment")
                 pass
 
             # Check if the species is Locally Rare Species or not based on the threshold passed by the user:
-            if ((len(occ_grid)/len(filtered_grid))*100) < cut_lrf:
+            if ((len(occ_grid)/len(grid))*100) < cut_lrf:
                 print(f"La especie/taxon {specie} es Locally Rare Feature!")
                 lrs_array.append(specie)
 
                 # Add a value of 5 into a column 'aggregation' where the the especies is present
-                filtered_grid.loc[occ_grid.index, "aggregation"] += 5
+                grid.loc[occ_grid.index, "aggregation"] += 5
                 pass
 
-            elif ((len(occ_grid)/len(filtered_grid))*100) >= cut_lrf:
+            elif ((len(occ_grid)/len(grid))*100) >= cut_lrf:
                 print(f"La especie/taxon {specie} NO es Locally Rare Feature!")
                 pass
 
@@ -249,9 +283,26 @@ def aq1(
             pass
 
     # Average the value with the number of Locally Rare Features
-    filtered_grid['aq1'] = filtered_grid["aggregation"]/len(lrs_array)
+    grid['aq1'] = grid["aggregation"]/len(lrs_array)
 
-    return filtered_grid
+    return grid
 
-aq1_gdf = aq1(r"C:\Users\beñat.egidazu\Desktop\Tests\EVA\cantabria_test.geojson", ["Spartina", "Anas", "Halimione"], grid_size=3000, min_grid_per=5, cut_lrf=30, span_years=50)
+
+# Testing AQ1:
+aoi = r"C:\Users\beñat.egidazu\Desktop\Tests\EVA\cantabria.geojson"
+grid_size = 3000
+grid = create_grid(aoi = aoi, grid_size=grid_size)
+aq1_gdf = aq1(aoi, ["Spartina", "Anas", "Halimione"], grid= grid, min_grid_per=5, cut_lrf=30, span_years=50)
+
+aq1_gdf.to_file(r"C:\Users\beñat.egidazu\Desktop\Tests\EVA\cantabria_test_aq1.geojson")
+
+
+# Assessment Question 2: abundance of Locally Rare Features (I could not find the way of retrieving abundance data from pyobis)
+def aq2():
+    return 
+
+# Assessment Question 5: presence of Nationally Rare Features/Species
+# def aq5(
+        
+# ) -> pd.DataFrame
 
